9e2046994dbefc73bebaf73a591d772ae0de19fd
NLarrea <larrea.naia@gmail.com>
2022-11-24 12:47:16 +0100

    01-introduction/ added

---
+++ 01-introduction/introduction.R
@@ -0,0 +1,92 @@
+# esto es un comentario, puedes escribir lo que quieras
+# para crear un comentario, usa: '#'
+
+# con la función print() se pueden imprimir los valores por pantalla
+
+
+
+# VARIABLES
+# para crear variables se usa '=' o '<-' (el segundo se usa más en R)
+# las variables en R son "case-sensitive", es decir, que "num" y "Num"
+#son dos variables distintas
+
+
+
+# TIPOS DE DATOS
+# numéricos (con comas)
+var1 <- 3.14
+
+# integers (numéricos pero sin comas) -> poner un 'L' al final para indicar que es integer
+var <- 88L
+
+# texto (strings) -> tienen que ir entre comillas dobles (") o simples (')
+var3 <- "hola"
+var4 <- 'hola'
+# si se quieren representar unas comillas: \" y usar cat() para imprimir
+print("Esto imprime \"las barras\" y las comillas")
+cat("Esto no imprime \"las barras\", solo las comillas")
+
+# booleanos -> o verdadero (TRUE), o falso (FALSE)
+
+
+
+# MATEMÁTICAS BÁSICAS
+# variables de ejemplo:
+x <- 11
+y <- 4
+# OPERADORES ARITMÉTICOS
+# suma y resta
+print(x + y) # 11+4 = 15
+print(x - y) # 11-4 = 7
+
+# multiplicación y división
+print(x * y) # 11*4 = 44
+print(x / y) # 11/4 = 2.75
+
+# potencias -> 2 formas de hacerlo
+print(x ^ y) # forma 1: 11^4 = 14641
+print(x ** y) # forma 2: 11**4 = 14641
+
+# módulo (resto de la división)
+print(x %% y) # 11%%4 = 3
+
+# división de integers (sin comas)
+print(x %/% y) # 11%/%4 = 2
+
+# funciones min() y max()
+print(min(x, y)) # imprime el pequeño de los dos: 4
+print(max(x, y)) # imprime el mayor de los dos: 11
+
+# raíz cuadrada -> sqrt()
+print(sqrt(64)) # 8
+
+
+
+# RELATIONAL OPERATORS
+# > mayor que
+# < menor que
+# >= mayor o igual que
+# <= menor o igual que
+# == igual que
+# != diferente a
+print(4 < 2) # FALSE
+print (3 >= 3) # TRUE
+
+
+
+# INTRODUCIR DATOS MANUALMENTE / PEDIR DATOS AL USUARIO
+input <- readLines('stdin')
+
+# ahora input es una variable que guarda TODO lo que el usuario introduzca
+#al ejecutar el programa.
+
+# si quieres acceder al primer elemento: input[1]
+# si quieres acceder al segundo elemento: input[2] (y así sucesivamente)
+
+# 'stdin' entre paréntesis es obligatorio (Standard Input)
+
+# todos los datos guardados en "input" se guardarán como STRINGS, si quieres
+#que esos datos se comporten como números:
+x <- as.integer(input[1])
+# ahora sí, puedes usar el dato de input[1] como un número:
+print(x * 2)
