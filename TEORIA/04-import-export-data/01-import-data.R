e8f5087451de101776502927f2f3a9a32a4a44d1
NLarrea <larrea.naia@gmail.com>
2022-11-26 01:33:51 +0100

    TEORIA/04-import-export-data/ added

---
+++ TEORIA/04-import-export-data/01-import-data.R
@@ -0,0 +1,115 @@
+# IMPORTAR DATOS
+
+# DÓNDE ESTAMOS TRABAJANDO
+# ver en qué directorio está trabajando R
+getwd()
+
+# interesante trabajar en el directorio desde el que se van a importar datos
+# cambiamos la ubicación en la que está trabajando R:
+setwd("C:/Users/larre/Documents/NAIA/programacion/R-desde-cero/04-analyzing-data")
+getwd() # para comprobar que se ha cambiado el directorio
+
+
+
+# IMPORTAR DATOS DE ARCHIVO .txt CON COMANDOS -> read.table()
+# primero cambio el directorio por haber añadido carpeta para archivos
+setwd("C:/Users/larre/Documents/NAIA/programacion/R-desde-cero/04-analyzing-data/data-to-import")
+
+# guardamos en "dataCostesTxt" los datos importados desde el archivo costes.txt
+dataCostesTxt <- read.table(file = "costes.txt", header = TRUE, dec = ".", sep = "\t")
+
+# file = "archivo desde el que leer"
+#   si no estuviéramos trabajando en el directorio donde se encuentra "costes.txt",
+#   habría que haber indicado en "file" el directorio en vez de solo el nombre
+#   del archivo
+# header = TRUE o FALSE. Indica si los datos tienen una cabecera (en este caso, sí)
+# dec = con qué se indican las comas en los números: "." (en este caso), o ","
+# sep = con qué se han separado los datos a importar en el archivo .txt
+#     "\t" indica que se ha usado el tabulador
+
+# imprimimos los datos:
+dataCostesTxt
+# se imprime lo siguiente:
+#   costo_unit costo_mat costo_mano_de_obra
+# 1      13,59        87                 80
+# 2      15,71        78                 95
+# 3      15,97        81                106
+# 4      20,21        65                115
+# 5      24,64        51                120
+
+summary(dataCostesTxt) # podemos ver un "resumen" de lo que se ha importado
+
+
+
+# ACCEDER A LOS DATOS DEL ARCHIVO .txt -> forma 1: comandos
+# (forma 2: después de acceder a archivos Excel)
+var(dataCostesTxt$costo_unit) # dará error -> no es accesible
+
+# vamos a hacer los datos accesibles de forma "global" convirtiendo dataCostesTxt
+#en un data frame:
+attach(dataCostesTxt)
+
+# imprimir los nombres de las variables creadas
+names(dataCostesTxt) # imprime: "costo_unit" "costo_mat" "costo_mano_de_obra" 
+
+# ahora sí se pueden calcular y mostrar valores
+print(costo_mat) # imprime: 87 78 81 65 51
+
+mean(costo_mat) # 72.4
+
+# ahora sí va a funcionar
+var(costo_mat) # 207.8
+
+# EN VEZ DE USAR ATTACH, se podría usar: $
+dataCostesTxt$costo_mat # mostraría: 87 78 81 65 51
+
+class(costo_mat) # integer
+
+
+
+# IMPORTAR DATOS DESDE UN ARCHIVO DE EXCEL
+# para leer de un archivo Excel, hay que instalar y activar algunos paquetes:
+
+# PASO 1: comprobar librerías
+library() # debemos ver si tenemos "readxl" y "openxlsx" instalados
+# los nombres de ambas librerías deberían aparecer en la columna izquierda
+
+# si no están instalados -> PASO 2
+# si están instalados -> PASO 3
+
+# PASO 2: instalar las librerías
+# Ir a "Packages" en la ventana inferior derecha de RStudio y clicar en Install
+# Escribir primero el nombre de una librería (readxl, por ejemplo) y clicar
+#en instalar. Repetir con la otra librería (openxlsx).
+
+# PASO 3: activar las librerías
+# FORMA 1
+# Acceder a "Packages" en la ventana inferior derecha de RStudio.
+# Buscar y activar el paquete "readxl" clicando en el checkbox de la izquierda
+# Buscar y activar el paquete "openxlsx" de la misma forma
+# FORMA 2
+library(readxl)
+library(openxlsx)
+
+
+
+# ACCEDER A LOS DATOS DEL ARCHIVO Excel  -> forma 1: comandos
+dataCostesXLSX <- read_excel("costes.xlsx")
+
+dataCostesXLSX # visualizar que se haya importado bien
+# imprime lo siguiente:
+#   costo.unit costo.mat costo.mano.de.obra
+#        <dbl>     <dbl>              <dbl>
+# 1       13.6        87                 80
+# 2       15.7        78                 95
+# 3       16.0        81                106
+# 4       20.2        65                115
+# 5       24.6        51                120
+
+
+
+# ACCEDER A LOS DATOS DEL ARCHIVO Excel -> forma 2: panel environment
+# Ir a la ventana superior derecha de RStudio y clicar en "Import Dataset"
+# Seleccionar:
+#   "From Text(base)" para archivos .txt
+#   "From Excel" para archivos Excel
---
+++ TEORIA/04-import-export-data/02-export-data.R
@@ -0,0 +1,39 @@
+# EXPORTAR DATOS
+# RECORDATORIO -> NECESARIO TENER LAS LIBRERÍAS SIGUIENTES ACTIVAS:
+#   - readxl
+#   - openxlsx
+# RECORDATORIO -> PREFERIBLE ESTAR TRABAJANDO EN EL DIRECTORIO DE TRABAJO DONDE
+#SE VAN A IMPORTAR / EXPORTAR LOS DATOS
+#   - getwd() ->
+#   - setwd() ->
+
+# se pueden exportar los datos guardados en un data frame tanto a formato .txt,
+#como a Excel
+
+# EJEMPLO
+# creamos un data frame
+# 1. creamos los vectores
+num <- 1:10 # es lo mismo que hacer: c(1:10), o: c(1,2,3,4,5,6,7,8,9,10)
+num_cuadrado <- num ^ 2
+num_cubo <- num ^ 3
+
+# 2. creamos el data frame
+numeros <- data.frame(num, num_cuadrado, num_cubo)
+
+# vemos el data frame:
+numeros
+
+# como la data frame la ha creado R, no es necesario hacer "attach()" para
+#acceder a los datos de forma individual:
+num
+mean(num_cuadrado)
+
+
+
+# EXPORTAR A .txt
+write.table(numeros, file = "exported-data/numeros-exportados.txt", sep = "\t", dec = ".", row.names = FALSE, col.names = TRUE)
+
+
+
+# EXPORTAR A Excel
+write.xlsx(numeros, file = "exported-data/numeros-exportados.xlsx")
