9c0d8388d07ab01cfd3c38e94a7815a2c925671d
NLarrea <larrea.naia@gmail.com>
2022-12-13 00:48:43 +0100

    EJERCICIOS/01-ejercicios-basicos/ error fixed

--- EJERCICIOS/01-ejercicios-basicos/01-secuencia-fibonacci.R
+++ EJERCICIOS/01-ejercicios-basicos/01-secuencia-fibonacci.R
@@ -1,5 +1,20 @@
-i <- 1
-while(i <= 5){
-  print(i)
+fib <- function(i, f1, f2){
+  if(i == 0) return(0)
+  else if(i == 1) return(1)
+  return(f1 + f2)
+}
+
+fib1 <- 0
+fib2 <- 0
+i <- 0
+
+num <- readline(prompt = "Introduce un número entero: ")
+
+while(i < num){
+  actualFib <- fib(i, fib1, fib2)
+  print(actualFib)
+
+  fib2 <- fib1
+  fib1 <- actualFib
   i <- i + 1
 }
